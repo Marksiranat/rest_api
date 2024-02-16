8d2f47565fb87c4f265fe9e87bb3590b8961f623
Marksiranat <siranat2546@gmail.com>
2024-02-16 10:38:36 +0700

    books_id

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const books = require('./db')
 app.get('/books', (req, res) => {
   res.json(books)
 })
+
+app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+  })
+
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
