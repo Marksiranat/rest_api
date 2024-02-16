7cb320a299b4435d53f85a929cad16809352b827
Marksiranat <siranat2546@gmail.com>
2024-02-16 10:57:09 +0700

    body-parser

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 // server.js
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-const books = require('./db')
-app.get('/books', (req, res) => {
-  res.json(books)
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/books/:id', (req, res) => {
-    res.json(books.find(book => book.id === req.params.id))
-  })
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+const books = require("./db");
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+
+app.get("/books/:id", (req, res) => {
+  res.json(books.find((book) => book.id === req.params.id));
+});
 
 app.listen(3000, () => {
-  console.log('http://localhost:3000')
-})
+  console.log("http://localhost:3000");
+});
